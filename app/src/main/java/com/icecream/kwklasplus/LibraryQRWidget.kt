f08e8aeb7f80cd1f99bca5da5d8f45b63d43f3e4
icecream0910 <taein2370@naver.com>
2024-10-12 01:54:20 +0900

    fix: 학생증 위젯 다크모드 적용 delay 개선

--- app/src/main/java/com/icecream/kwklasplus/LibraryQRWidget.kt
+++ app/src/main/java/com/icecream/kwklasplus/LibraryQRWidget.kt
@@ -1,5 +1,6 @@
 package com.icecream.kwklasplus
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
@@ -28,24 +29,16 @@ class LibraryQRWidget : AppWidgetProvider() {
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
     }
 }
 
+@SuppressLint("RemoteViewLayout")
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
     val views = RemoteViews(context.packageName, R.layout.library_q_r_widget)
-
-    // 다크 모드 여부에 따라 이미지 변경
-    val isDarkMode = context.resources.configuration.uiMode and
-            Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
-
-    val imageRes = if (isDarkMode) R.drawable.qr_widget_img_dark else R.drawable.qr_widget_img_light
-    views.setImageViewResource(R.id.widget_qr_code_img, imageRes)
-
     val intent = Intent(context, LibraryQRWidgetActivity::class.java)
     val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
     views.setOnClickPendingIntent(R.id.widget_qr_code_img, pendingIntent)
